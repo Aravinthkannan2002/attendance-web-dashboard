0c8b3caeb59c4b2633ca1ed56419b0449a514bab
Aravinthkannan <aravindkannan4614@gmail.com>
2026-03-27 13:00:52 +0530

    Deploy web dashboard

---
+++ .dart_tool/flutter_build/d3e6eb5580b21e1d0d9713946398ecbc/web_resources.d
@@ -0,0 +1,1 @@
+ C:\\Users\\91890\\OneDrive\\Desktop\\attendance_web_dashboard\\build\\web\\manifest.json:  C:\\Users\\91890\\OneDrive\\Desktop\\attendance_web_dashboard\\web\\index.html C:\\Users\\91890\\OneDrive\\Desktop\\attendance_web_dashboard\\web\\manifest.json
